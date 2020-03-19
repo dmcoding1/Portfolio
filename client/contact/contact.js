ea238bfa60e0fb7aa4a01075b88d6ebb3072a93d
Damian <damiandlu@interia.pl>
2020-03-19 19:57:32 +0100

    refactor scss code and add captcha

--- client/contact/contact.js
+++ client/contact/contact.js
@@ -1,13 +1,12 @@
 window.onload = () => {
-
   const name = document.getElementById("name");
   const email = document.getElementById("email");
   const message = document.getElementById("message");
   const form = document.querySelector(".form");
 
-  form.addEventListener("submit", sendMessage);
+  form.addEventListener("submit", handleSubmit);
 
-  function sendMessage(e) {
+  function handleSubmit(e) {
     e.preventDefault();
 
     const messageData = {
@@ -16,19 +15,32 @@ window.onload = () => {
       message: message.value
     };
 
-    fetch("http://localhost:4000/message", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(messageData)
-    })
-      .then(() => {
-        window.location.href = "http://localhost:4000/"
-        name.value = "";
-        email.value = "";
-        message.value = "";
-      })
-      .catch(err => console.error(error));
+    verify(messageData);
   }
 };
+
+function verify(data) {
+  grecaptcha
+    .execute("6LcbiuIUAAAAAHI5QSoY5HNdSs_I-QQ8q6ZUqnAf", { action: "homepage" })
+    .then(token => {
+      data.captcha = token;
+      sendMessage(data);
+    });
+}
+
+function sendMessage(data) {
+  fetch("http://localhost:4000/message", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(() => {
+      window.location.href = "http://localhost:4000/";
+      name.value = "";
+      email.value = "";
+      message.value = "";
+    })
+    .catch(err => console.error(error));
+}
