0f2ebb8d05388fb5a9543aad035a3a395bd4b74a
Damian <damiandlu@interia.pl>
2020-06-02 09:09:36 +0200

    add projects links

--- client/src/scripts/projectsData.js
+++ client/src/scripts/projectsData.js
@@ -6,7 +6,7 @@ const projectsData = [
     description:
       "Restaurant website with table reservation module. Scroll animations made with Intersection Observer API.",
     imageUrl: "../images/restaurant.jpg",
-    link: "#",
+    link: "http://51.178.42.224:3100/",
     githubLink: "https://github.com/dmcoding1/Restaurant",
     figmaLink:
       "https://www.figma.com/proto/ekrrtTF8YWDEqGQi6JHAtq/Restaurant?node-id=137%3A2&scaling=min-zoom",
@@ -29,7 +29,7 @@ const projectsData = [
     description:
       "Card generator with 25 palettes and 10 font pairs to choose from. Images are provided by Unsplash API. Card download and custom image upload features.",
     imageUrl: "../images/card-generator.jpg",
-    link: "#",
+    link: "https://christmas-cards-generator.netlify.app/",
     githubLink: "https://github.com/dmcoding1/Christmas-Card-Generator",
     figmaLink:
       "https://www.figma.com/proto/U0dxKIPpmw13zZywOdKmzI/Christmas-Card-Generator?node-id=76%3A4&scaling=scale-down",
@@ -52,7 +52,7 @@ const projectsData = [
     description:
       "Progressive Web App that displays notes, chords with keyboard and guitar sounds of selected music scale.",
     imageUrl: "../images/scales.jpg",
-    link: "#",
+    link: "https://musical-scales.netlify.app/",
     githubLink: "https://github.com/dmcoding1/Scales-App",
     tools: ["html5", "css3", "sass", "javascript", "webpack", "workbox"],
   },
@@ -63,7 +63,7 @@ const projectsData = [
     description:
       "App displays random quote from database, with searching quote by author and adding quote to database features.",
     imageUrl: "../images/quotes.jpg",
-    link: "#",
+    link: "http://51.178.42.224:5000/",
     githubLink: "https://github.com/dmcoding1/Quotes-Application",
     figmaLink:
       "https://www.figma.com/proto/xjKt0pgGfAvyY1zoe0ika1/Quotes?node-id=1%3A2&scaling=scale-down",
